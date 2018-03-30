42eb285d82b364d9a79a98e0f83814eeb58d2527
michellele994 <michellele@email.arizona.edu>
2018-03-30 00:45:56 -0700

    Working server and react

---
+++ client/src/pages/Articles/Articles.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+// import DeleteBtn from "../../components/DeleteBtn";
+// import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+// import { Col, Row, Container } from "../../components/Grid";
+// import { List, ListItem } from "../../components/List";
+// import { Input, TextArea, FormBtn } from "../../components/Form";
+
+class Articles extends Component {
+  state = {
+    articles: [],
+    title: "",
+    date: "",
+    url: ""
+  };
+
+  componentDidMount() {
+    this.loadArticles();
+  }
+
+  loadArticles = () => {
+    API.getArticles()
+      .then(res =>
+        this.setState({ articles: res.data, title: "", date: "", url: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  // deleteArticle = id => {
+  //   API.deleteArticle(id)
+  //     .then(res => this.loadArticles())
+  //     .catch(err => console.log(err));
+  // };
+
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.title && this.state.author) {
+  //     API.saveBook({
+  //       title: this.state.title,
+  //       author: this.state.author,
+  //       synopsis: this.state.synopsis
+  //     })
+  //       .then(res => this.loadBooks())
+  //       .catch(err => console.log(err));
+  //   }
+  // };
+
+  render() {
+    return (<div>
+              <h1>Articles On My List</h1>
+            {this.state.articles.length ? (
+                this.state.articles.map(article => (
+                      <strong>
+                        {article.title} by {article.date} url {article.url}
+                      </strong>
+                ))
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            </div>
+    );
+  }
+}
+
+export default Articles;
---
+++ routes/articles.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.get("/recipes", (req, res) => {
+  axios
+    .get("https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=5ebc6c97b9d54381b6f387bfc06b79dd", { params: req.query })
+    .then(({ data: { response } }) => res.json(response))
+    .catch(err => res.status(422).json(err));
+});
+
+module.exports = router;
